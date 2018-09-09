771b3adb945d4b1e8826597c76d2d845ebdb578e
Hugo <hugocoutinho2011@gmail.com>
2018-09-09 19:16:09 -0300

    Queue and LinkedList

---
+++ LinkedList.js
@@ -0,0 +1,159 @@
+//dynamic data structure
+function LinkedList() {
+
+    //class node
+    let Node = function (element) {
+        this.element = element;
+        this.next = null;
+    };
+    let length = 0;
+    let head = null;
+
+    //add element in the last position
+    this.append = function (element) {
+        let node = new Node(element),
+            current;
+
+        if (head == null) {
+            //add the first element for the list.
+            head = node;
+        } else {
+            current = head;
+            //iteration until the next current is null.
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        length++;
+    };
+
+    this.removeAt = function (position) {
+
+        //verify if the position it is a possible position to remove .
+        if ((position >= -1) && (position <= length)) {
+            let current = head
+            let previous
+            let index = 0;
+            let removed = false;
+
+            if (position == 0) {
+                if (current && current.next) {
+                    //remove at first position, so the head will set the next current(passing throught the current "current")
+                    head = current.next;
+                    removed = true;
+                }
+            } else {
+                //will be road it while the position is not get.
+                while (index++ < position) {
+                    //the variable "previous" is set to the variable "current" value, so then "current" is set to the next current value
+                    previous = current;
+                    current = current.next;
+                }
+                //that way we already have the we want remove it (the current value), 
+                //so it is simple, just make the "previous.next" seting to "current.next" passing througt the "current" value
+                removed = true;
+                previous.next = current.next;
+            }
+            if (removed) {
+                length--;
+                console.log("removeAt: successfully- " + current.element);
+            } else {
+                console.log("removeAt: is empty");
+            }
+        } else { console.log("removeAt: number is not rigth- " + position); }
+
+    }
+
+    this.appendAt = function (element, position) {
+
+        let node = new Node(element);
+        let current = head;
+        let index = 0;
+        let previous;
+
+        if (position > -1 && position <= length) {
+
+            if (position == 0) {
+                node.next = current;
+                head = node;
+            } else {
+                while (index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+            }
+            length++;
+            return toString();
+
+        } else { console.log("appendAt: number is not rigth- " + position); }
+
+
+    }
+
+    this.toString = function () {
+
+        let current = head;
+        string = '';
+        while (current) {
+            string += current.element + (current.next ? ', ' : '');
+            current = current.next;
+        }
+        return "iteration with list- " + string;
+    }
+
+    this.remove = function (element) {
+
+        let index = this.indexOf(element);
+        return this.removeAt(index);
+    }
+
+
+    this.indexOf = function (element) {
+
+        let current = head;
+        let index = 0;
+
+        while (current) {
+            if (current.element == element) {
+                console.log("indexOf: position = " + index + " element= " + element);
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        console.log("indexOf: element does not exist in your linkedList");
+    }
+
+    this.isEmpty = function () {
+        return length == 0;
+    }
+
+    this.size = function () {
+        console.log("size of list- " + length);
+    }
+
+    this.getHead = function () {
+        let getHead = head;
+        do {
+            console.log("Head: " + getHead.element);
+            getHead = getHead.next;
+        } while (getHead)
+    }
+}
+
+let shopList = new LinkedList();
+shopList.append("banana");
+shopList.append("iphone");
+shopList.append("tv");
+shopList.appendAt("headPhone", 2);
+shopList.removeAt(0);
+shopList.indexOf("tv");
+shopList.remove("tv");
+shopList.size();
+console.log("List is really empty? " + shopList.isEmpty());
+console.log(shopList.toString());
+shopList.getHead();
+
---
+++ Queue.js
@@ -0,0 +1,67 @@
+//JavaScript code for data structure - queue
+//fifo - first in, first out
+
+function Queue() {
+
+    let items = new Array();
+
+    this.enqueue = function (element) {
+        items.push(element);
+    }
+
+    this.dequeue = function () {
+        //remove de first element in array
+        return 'first element in array was removed: ' + items.shift();
+    }
+
+    this.front = function () {
+        return 'front: ' + items[0];
+    }
+
+    this.isEmpty = function () {
+        var bol = items.length == 0;
+        return 'is empty? ' + bol;
+    }
+
+    this.size = function () {
+
+        // here we have a problem, in this method my goal is return the size of the queue,
+        // but in no one minute we think about push method problem, yes,
+        //  because is if the user wanna pushing in queue a array of family rather than one person?? 
+        // the better solution i find is using the experimental tech 'flat', 
+        // using it i can get the length inside the array pushed and the length of persons in the queue.
+        let size = items.flat().length;
+        return 'length: ' + size;
+    }
+
+    this.print = function () {
+        console.log('to string: ' + items.toString());
+    }
+
+}
+
+// here it is a experimental tech , i am using it to get the size of the queue.how it is a 
+// experimental,so, I need to have my own function instead. and then that it is.
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function (depth = 1) {
+        return this.reduce(function (flat, toFlatten) {
+            return flat.concat((Array.isArray(toFlatten) && (depth - 1)) ? toFlatten.flat(depth - 1) : toFlatten);
+        }, []);
+    }
+});
+
+//into terminal type "node"
+//console - 
+bankQueue = new Queue();
+var family = ['Hugo', 'Gabi', 'Levi', 'July', 'Maria'];
+bankQueue.enqueue('ronaldinho');
+bankQueue.enqueue('romario');
+bankQueue.enqueue(family);
+bankQueue.enqueue('neymar');
+
+bankQueue.print();
+console.log(bankQueue.size())
+console.log(bankQueue.isEmpty())
+console.log(bankQueue.front())
+console.log(bankQueue.dequeue())
+bankQueue.print();
